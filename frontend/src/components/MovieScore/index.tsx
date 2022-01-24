ba0c4841ff7871c2305a59618bee48d8d7ec2eb1
AurelianoAlencar <alencarsobreira@terra.com.br>
2022-01-24 20:45:29 -0300

    Show score

--- frontend/src/components/MovieScore/index.tsx
+++ frontend/src/components/MovieScore/index.tsx
@@ -1,18 +1,30 @@
 
 import MovieStars from "components/MovieStars";
+import { type } from "os";
 import './styles.css';
-function MovieScore() {
 
-     const score = 3.5;
-     const count = 13;
+
+type Props = {
+
+    score : number;
+    count : number;
+}
+
+
+
+function MovieScore({score , count } : Props) {
+
+    
 
     return (
 
 
 
         <div className="dsmovie-score-container">
-            <p className="dsmovie-score-value">{score > 0 ? score.toFixed(1) : '-'}</p>
-            <MovieStars />
+
+                  
+            <p className="dsmovie-score-value">{score > 0 ? score.toFixed(1) : '-'}</p>    // caminho caso não tenha avalição, ficará mostrando um traço.
+            <MovieStars  score ={score}/>                                                 // se score maior que zero score.toFixed, mostra casa decimal 
             <p className="dsmovie-score-count">{count} avaliações</p>
         </div>
 
